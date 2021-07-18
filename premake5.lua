8bef3d8b82916e2b24f8520ac84741abb21ddef1
Luke <lukesnapacc@gmail.com>
2021-07-18 15:53:54 +0200

    Added premake and generated projects

---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "Charlie"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Charlie"
+	location "Charlie"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.17134.0"
+
+		defines
+		{
+			"CL_PLATFORM_WINDOWS",
+			"CL_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "CL_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "CL_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "CL_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Charlie/vendor/spdlog/include",
+		"Charlie/src"
+	}
+
+	links
+	{
+		"Charlie"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.17134.0"
+
+		defines
+		{
+			"CL_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "CL_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "CL_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "CL_DIST"
+		optimize "On"
